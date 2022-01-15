79e80958b5a8fbed1dbb42d532bba75f4634b391
AYORINDE TIMILEHIN <38140090+timilxr@users.noreply.github.com>
2022-01-16 00:07:33 +0100

    first commit: version 1 roll out

---
+++ pages/posts/first-post.js
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import Head from 'next/head';
+// import Script from 'next/script'
+import Layout from '../../components/layout'
+
+
+const FirstPost = () => {
+    return (
+        <Layout>
+            <Head>
+                <title>First post</title>
+            </Head>
+            {/* <Script 
+            src='https://connect.facebook.net/en_US/sdk.js'
+            strategy='lazyOnload'
+            onLoad={()=>console.log(`script loaded correctly, window.FB has been populated`)}
+            /> */}
+            <h1>First Post</h1>
+            <h2>
+                <Link href='/'>
+                    <a>Back to home</a>
+                </Link>
+            </h2>
+        </Layout>
+    )
+}
+
+export default FirstPost;
